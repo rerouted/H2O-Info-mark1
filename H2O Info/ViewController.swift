cefd411c76cf33a374c08d49804239fa1ceb90a8
Ryan Sullivan <ryan.sullivan@charter.com>
2018-07-11 09:35:57 -0400

    Initial Commit

---
+++ H2O Info/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  H2O Info
+//
+//  Created by Sullivan, Ryan P on 7/11/18.
+//  Copyright © 2018 Sullivan, Ryan P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
